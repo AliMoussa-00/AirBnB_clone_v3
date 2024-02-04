9bc5b1b24bf4e4b679fd1f86c4f6d186a2f63088
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-04 11:59:41 -0800

    style : app - index

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 """defining the app module for the REST API"""
 
-from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from os import getenv
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from models import storage
-from os import getenv
+from api.v1.views import app_views
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+APP = Flask(__name__)
+APP.register_blueprint(app_views)
 
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+CORS = CORS(APP, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception=None):
+@APP.teardown_appcontext
+def teardown_appcontext():
     """close the storage session"""
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found(error):
+@APP.errorhandler(404)
+def not_found():
     """creating a custom response for code 404"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-    port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
-    app.run(host=host, port=port, threaded=True)
+    HOST = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
+    PORT = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
+    APP.run(host=HOST, port=PORT, threaded=True)

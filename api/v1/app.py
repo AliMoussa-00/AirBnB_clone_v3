a6764c745a8749f89877b6f6a7fe340b99ed3fdf
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-04 12:05:53 -0800

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,25 +7,25 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
-APP = Flask(__name__)
-APP.register_blueprint(app_views)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-CORS = CORS(APP, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
-@APP.teardown_appcontext
+@app.teardown_appcontext
 def teardown_appcontext():
     """close the storage session"""
     storage.close()
 
 
-@APP.errorhandler(404)
+@app.errorhandler(404)
 def not_found():
     """creating a custom response for code 404"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-    PORT = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
-    APP.run(host=HOST, port=PORT, threaded=True)
+    host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
+    port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
+    app.run(host=host, port=port, threaded=True)

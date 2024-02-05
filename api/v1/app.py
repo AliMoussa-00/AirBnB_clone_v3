67d8e27e687668abb58bd40a968ea379d68efeb2
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-05 02:08:21 -0800

    style app

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,7 @@ def teardown_appcontext(exception):
 @app.errorhandler(404)
 def not_found(error):
     """creating a custom response for code 404"""
-    return make_response(jsonify({'error': 'Not found'})), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":

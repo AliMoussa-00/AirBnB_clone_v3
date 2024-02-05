4c7348d0873ea6742e939cc802f8e2e9ecf0d36a
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-05 02:13:20 -0800

    change index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" Defining the index module"""
+"""Defining the index module"""
 
 from flask import jsonify
-
-from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -11,26 +9,23 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from api.v1.views import app_views
+
+classes = {'amenities': Amenity, 'cities': City,
+           'places': Place, 'reviews': Review,
+           'states': State, 'users': User}
 
 
 @app_views.route('/status')
 def get_status():
-    """get the status code """
-    return jsonify(status='OK')
+    """get the status code"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats')
 def get_count():
     """retrieves the number of each objects by type"""
 
-    classes = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
-    }
     objs = {}
     for k, v in classes.items():
         objs[k] = storage.count(v)
